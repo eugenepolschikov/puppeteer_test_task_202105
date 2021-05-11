6bc930bd0637419d0818a88fce294639f121616b
eugene polschikov <eugene.polschikov@gmail.com>
2021-05-11 17:31:04 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const puppeteer = require("puppeteer");
+
+
+(async function () {
+    const browser = await puppeteer.launch({headless:false});
+    const page = await browser.newPage();
+    await page.goto("https://pptr.dev");
+
+    await page.waitFor(5000);
+    await browser.close();
+})();
